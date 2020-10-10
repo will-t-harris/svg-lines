6e85048ca5e702d81311307a50e3dd0186ca83b6
Will Harris <will.t.harris@protonmail.com>
2020-10-10 11:42:05 -0700

    initial CRA cleanup & setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
+import { ReactElement } from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = (): ReactElement => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>App Component</h1>
       </header>
     </div>
   );
