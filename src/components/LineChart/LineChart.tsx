fdbeb253cab504a26e0f9991152273e00247e859
Will Harris <will.t.harris@protonmail.com>
2020-10-10 11:56:50 -0700

    feat: start linechart component

---
+++ src/components/LineChart/LineChart.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import "./LineChart.css";
+
+interface Props {
+	data: [];
+	color: string;
+	svgHeight: number;
+	svgWidth: number;
+}
+
+export const LineChart: React.FC<Props> = ({
+	data,
+	color = "#ff4500",
+	svgHeight = 200,
+	svgWidth = 600,
+}) => {
+	return <h1>Line Chart</h1>;
+};
