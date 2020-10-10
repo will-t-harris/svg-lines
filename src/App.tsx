545658304a15bebe57395a9e925e14506ad3af0a
Will Harris <will.t.harris@protonmail.com>
2020-10-10 11:52:19 -0700

    feat: define createRandomArray method

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import * as React from 'react';
-import { ReactElement } from 'react';
-import './App.css';
+import * as React from "react";
+import { ReactElement } from "react";
+import "./App.css";
 
 const App: React.FC = (): ReactElement => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>App Component</h1>
-      </header>
-    </div>
-  );
-}
+	const createRandomArray = ({ total = 10 }: { total: number }) => {
+		let data = [];
+		for (let element: number = 0; element < total; element++) {
+			const y: number = Math.floor(Math.random() * 50) + 50;
+			const obj: { x: number; y: number } = {
+				x: element,
+				y,
+			};
+			data.push(obj);
+		}
+		return data;
+  };
+  
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>App Component</h1>
+			</header>
+		</div>
+	);
+};
 
 export default App;
